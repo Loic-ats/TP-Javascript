2de8a6b9110d9ec35aa44e6bcf9535b2bc1e2d2a
unknown <you@example.com>
2020-09-16 12:31:24 +0200

    TP JAVASCRIPT Fonction et tableau finis

--- 01.function.js
+++ 01.function.js
@@ -8,11 +8,9 @@ var nombre2 = 20;
 }
 
 resultat1 = additionner(nombre1,nombre2);
-
 console.log("resultat1 = " + resultat1);
 
-var somme = function additionnner(nb1, nb2){return nb1 + nb2};
-
+var somme = additionner;
 var resultat2 = somme (nombre1,nombre2);
 console.log("resultat2 = " + resultat2);
 
@@ -28,6 +26,10 @@ function afficherOperation(nomOperation, operation, nb1, nb2) {
     return nomOperation + '(' + nb1  +',' + nb2 + ')' + '=' + operation; 
 }
 
+/*function afficherOperation(nomOperation, operation, nb1, nb2) {
+    consol.log (nomOperation + "(" + nb1 + "," + nb2 + ") =>" + operation( nb1,nb2)); 
+}*/
+
 var resultat4 = afficherOperation("somme", somme(20, 40),20,40);
 console.log (resultat4);
 
@@ -35,6 +37,5 @@ var resultat5 = afficherOperation("multiplication", multiple(10, 20),10,20);
 console.log (resultat5);
 
 var soustraction = function soustraction(nb1, nb2) {return nb1-nb2}; 
-
 var resultat6 = afficherOperation("soustraction", soustraction(15,5),15,5);
 console.log (resultat6);
--- 02.tableau.js
+++ 02.tableau.js
@@ -1,7 +1,7 @@
 var villes = ['nantes', 'paris','saint-nazaire', 'angers', 'le mans' ];
 
-villes.forEach(function(element) {
-    [console.log(element)];
+villes.forEach(function(element, index) {
+    [console.log("Ville" + index + ':' + element)];
 });
 
 var lettreADansToutesLesVille= function(element,index,villes)
@@ -13,3 +13,18 @@ var auMoinsUneVilleAvecUnTire= function(element,index,villes)
 {return element = element.includes('-');}
 
 console.log ("Le tableau contient au moins une ville avec un '-' " + villes.some(auMoinsUneVilleAvecUnTire));
+
+var villesSansTiretSansEsp= function(element,index,villes)
+{return element = !element.includes('-');}
+
+console.log ("Les villes qui ne contiennent pas de tiret ou d'espace sont : " + villes.filter(villesSansTiretSansEsp));
+
+console.table(
+    
+villes.filter(function(ville){
+return ville.endsWith ('s');
+
+}).map (function (ville) {
+    return ville.toUpperCase();
+    
+}));
